626aed6257f01f0e2061d63617216b2faf433435
Lucas <lucashnutn@gmail.com>
2019-08-27 00:02:22 -0300

    2.js variables, diferentes metodos, substr, charAt, toUpperCase, toLowerCase,

--- clase1.js
+++ clase1.js
@@ -1,11 +1,11 @@
 var nombre = 'Lucas'
 var apellido = 'Frias'
 
-var edad = 28
+var edad = 27
 
 var peso =28
 
-edad = '28 años'
+edad = '27 años'
 
 console.log('Hola ' + nombre + ' ' + apellido)
 console.log('Tengo '+ edad);
---
+++ clase2.js
@@ -0,0 +1,19 @@
+var nombre = 'Lucas'
+var apellido = 'Hernan'
+
+
+var nombreEnMayuscula = nombre.toUpperCase()
+
+var apellidoEnMinuscula = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido}`
+
+var str = nombre.substr(1,3)
+
+var ultimaLetraNombre = nombre.charAt(nombre.length-1)
+
+var ultimaLetra = nombre.charAt(cantDeLetrasDelNombre-1)
