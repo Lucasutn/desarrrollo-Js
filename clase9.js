241fd5a344aa9ba9f329878715395738026d3f6e
Lucas <lucashnutn@gmail.com>
2019-09-03 23:32:41 -0300

    9. comparaciones == y ===, var otraPersona= lucas, si modifico otraPersona tambien se modifica el objeto lucas, ya que otraPersona hace referencia a la direccion en memoria de lucas

---
+++ clase9.js
@@ -0,0 +1,16 @@
+var x = 4, y= '4'
+
+
+var sacha = {
+    nombre: 'Sacha'
+}
+
+// var otraPersona= sacha
+
+var otraPersona={
+    ...sacha
+}
+
+// var otraPersona = {
+//     nombre='sacha'
+// }
